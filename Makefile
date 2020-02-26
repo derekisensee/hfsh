d63ec74ed1e8b3533a0b7bb4322ed9b04df08cee
derek <derek@debian>
2020-02-25 23:11:01 -0600

    lots of changes!

--- Makefile
+++ Makefile
@@ -1,3 +1,60 @@
-all: hfsh
-hfsh: hfsh.c
-	gcc -o hfsh hfsh.c -lpthread
+#
+# Sample Makefile for C and C++ programming examples
+#
+CC = gcc
+CXX = g++ 
+CFLAGS = -Wall -Werror
+CXXFLAGS = -std=c++17 -Wall -Werror
+
+
+#
+# For this project...pick one file or the other (.c or .cpp)
+# Just uncomment out the one you want to use (or comment out the other one)
+#
+CSRCS   = hfsh.c  
+#CXXSRCS = hfsh.cpp
+
+
+# Create the object files and executable files
+OBJS_C    = $(CSRCS:.c=.o)
+OBJS_CXX  = $(CXXSRCS:.cpp=.o)
+OBJS      = $(OBJS_C) $(OBJS_CXX)
+
+PROGS_C   = $(CSRCS:.c=)
+PROGS_CXX = $(CXXSRCS:.cpp=)
+PROGS     = $(PROGS_C) $(PROGS_CXX)
+
+
+# This is the multi-executable target...
+# If you type 'make' at the command line it will build
+# all programs
+.PHONY: all clean
+
+
+all: $(PROGS)
+
+
+# Make all programs from object files
+#
+# Make sure you pick the right compile action in this target
+$(PROGS): %: %.o Makefile
+#	$(CC) $< -o $@
+	$(CXX) $< -o $@
+
+
+# This line will convert any .c file to a .o file.
+$(OBJS_C): %.o: %.c Makefile
+	$(CC) $(CFLAGS) -c $<
+
+
+# This line will convert any .cpp file to a .o file
+$(OBJS_CXX): %.o: %.cpp Makefile
+	$(CXX) $(CXXFLAGS) -c $<
+
+
+# Type 'make clean' up the subdirectory...
+# Be Very Careful Not To Haphazzardly Make Changes!!!!!!!
+clean:
+	$(RM) $(PROGS) $(OBJS) 
+
+
--- hfsh.c
+++ hfsh.c
@@ -2,32 +2,86 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <string.h>
+#include <unistd.h>
 
-void *extcmd(void *vargp) {
-  //printf(vargp[0]);
+char *path;
+
+void *extcmd(char *cmd, char *ext) {
+  // since strtok overwrites old string, have to make copy
+  char *tempEnv;
+  tempEnv = malloc(strlen(path)+1); 
+  strcpy(tempEnv, path);
+  strtok(tempEnv, ";");
+  // have to figure out how exactly to split path paths
+  char *splitInput = strtok(tempEnv, ";"); // strtok modifies 'buffer' ?
+
+  while (splitInput != NULL) {
+    // if command does not execute
+    if (execv(strcat(splitInput, cmd), ext) == -1) {
+      splitInput = strtok(NULL, ";");
+    }
+  }
+  
+
+  free(tempEnv);
 }
 
-int main(int argc, char *args[]) {
-  // normal mode
-  if (argc == 1) {
-    int running = 1;
+void errorPrint() {
+  char error_message[30] = "An error has occurred\n";
+  write(STDERR_FILENO, error_message, strlen(error_message)); 
+}
 
-    while (running) {
+void interactiveMode() {
+  int running = 1;
+
+    while (running == 1) {
       // vars
       char *buffer;
       size_t buffSize = 32;
-      size_t input;
+      //size_t input;
       buffer = (char *)malloc(buffSize * sizeof(char));
 
       // check for any buffer errors?
       // --------------------
 
       printf("hfsh> ");
-      input = getline(&buffer, &buffSize, stdin); // input is stored in 'buffer'
-      char * splitInput = strtok(buffer, " "); // strtok modifies 'buffer' ?
-      printf("cmd: %s\n", splitInput);
-      //printf("\n input: %s", buffer);
+      getline(&buffer, &buffSize, stdin);
+      char *splitInput = strtok(buffer, " \t\n"); // strtok modifies 'buffer' ?
+
+      // built-in commands
+      if (strcmp(splitInput, "exit") == 0) {
+        exit(0);
+      }
+
+      if (strcmp(splitInput, "cd") == 0) {
+        int argCount = 0;
+        
+        while (splitInput) {
+          if (argCount++ > 1) {
+            errorPrint();
+            break;
+          }
+          chdir(splitInput);
+        }
+      }
+
+      if (strcmp(splitInput, "path")) {
+
+      }
+
+      else {
+        extcmd(splitInput, strtok(NULL, ""));
+      }
+
+      // -----------------
     }
+}
+
+int main(int argc, char *args[]) {
+  path = "/bin";
+  // normal mode
+  if (argc == 1) {
+    interactiveMode();
   }
   // batch mode
   if (argc == 2) {
